55af20977e9865b3551bc29ba54accddfc2a188b
LeThiTuongOanh <134386170+LeThiTuongOanh@users.noreply.github.com>
2024-08-08 15:41:55 +0700

    Buoi4_Bai15: Viết hàm tính An sử dụng kỹ thuật đệ quy.

---
+++ Buổi 4/Code_BTVN/Buoi4_Bai15.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+
+// Hàm đệ quy tính tổng các giá trị của dãy từ A_1 đến A_n-1
+int tinhTongAn(int n);
+
+// Hàm đệ quy tính giá trị A_n
+int tinhAn_DeQuy(int n) {
+	if (n == 1) {
+		return 1; // Điều kiện cơ sở
+	}
+	else {
+		return n * tinhTongAn(n - 1);
+	}
+}
+
+// Hàm đệ quy tính tổng các giá trị của dãy từ A_1 đến A_n-1
+int tinhTongAn(int n) {
+	if (n == 1) {
+		return 1; // Điều kiện cơ sở
+	}
+	else {
+		return tinhAn_DeQuy(n) + tinhTongAn(n - 1);
+	}
+}
+
+int main() {
+	int n;
+	printf("Nhap gia tri cua n: ");
+	scanf_s("%d", &n);
+
+	int result = tinhAn_DeQuy(n);
+	printf("Gia tri A_%d la: %d\n", n, result);
+
+	getchar();
+	getchar();
+
+	return 0;
+}
